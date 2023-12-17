05fc98f14a021138a8e44ddb5999bb730bd7695b
kenken1199 <nakamori.k@gmail.com>
2023-12-17 12:24:09 +0900

    OK

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -2,13 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
-import Display from "./components/Display";
+// import Display from "./components/Display";
 import { User } from "./types/types";
 
 function App() {
   const [users, setUsers] = useState<User[]>([]);
 
-  const asyncFunc = async () => {
+  const handleClick = async () => {
     const res = await fetch("https://jsonplaceholder.typicode.com/todos");
     const responce = await res.json();
     console.log(responce);
@@ -27,7 +27,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => asyncFunc()}>Push!!</button>
+        <button onClick={handleClick}>Push!!</button>
         <p>
           Edit <code>src/App.tsx</code> and save to test HMR
         </p>
@@ -36,7 +36,11 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
       {users.map((post: User) => {
-        <Display key={post.id} {...post} />;
+        return (
+          <div key={post.id}>
+            <h2>{post.title}</h2>
+          </div>
+        );
       })}
     </>
   );
